ac60c4b0c3cfe21357f6cb5aad215a47fc18ca55
leebeanbin <wjdqlsdy1004@naver.com>
2024-09-02 18:23:42 +0900

    init

---
+++ src/main/java/com/sparta/internsecurity/InternSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.internsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternSecurityApplication.class, args);
+    }
+
+}
