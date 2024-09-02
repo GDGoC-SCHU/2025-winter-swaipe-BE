72202e12757330d6f703a42d2525f997c07bb803
leebeanbin <wjdqlsdy1004@naver.com>
2024-09-02 18:25:39 +0900

    add userrole set up

---
+++ src/main/java/com/sparta/internsecurity/user/enums/UserRole.java
@@ -0,0 +1,15 @@
+package com.sparta.internsecurity.user.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    USER("ROLE_USER"),
+    MANAGER("ROLE_MANAGER");
+
+    private final String authority;
+
+    UserRole(String authority) {
+        this.authority = authority;
+    }
+}
