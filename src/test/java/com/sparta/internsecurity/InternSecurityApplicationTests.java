3df80a8b2214fff78de1957b4ab7fadb9a1b7f18
leebeanbin <wjdqlsdy1004@naver.com>
2024-09-02 18:26:24 +0900

    remove error-able file

---
+++ src/test/java/com/sparta/internsecurity/InternSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.sparta.internsecurity;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class InternSecurityApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
