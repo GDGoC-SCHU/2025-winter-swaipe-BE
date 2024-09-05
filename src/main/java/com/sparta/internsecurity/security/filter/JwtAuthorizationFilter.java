7b7972afdcf62115a3e2299f21cce3eb6f90d53b
leebeanbin <wjdqlsdy1004@naver.com>
2024-09-05 16:48:13 +0900

    인가 단계의 필터링 uri 추가

--- src/main/java/com/sparta/internsecurity/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/internsecurity/security/filter/JwtAuthorizationFilter.java
@@ -2,11 +2,13 @@ package com.sparta.internsecurity.security.filter;
 
 
 import static com.sparta.internsecurity.security.jwt.JwtProvider.AUTHORIZATION_HEADER;
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.internsecurity.ErrorResponse;
 import com.sparta.internsecurity.security.details.UserDetailsServiceImpl;
 import com.sparta.internsecurity.security.jwt.JwtProvider;
+import com.sparta.internsecurity.user.dto.AccessTokenDto;
 import com.sparta.internsecurity.user.enums.UserRole;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -38,29 +40,43 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             FilterChain filterChain)
             throws ServletException, IOException {
 
-        if (req.getRequestURI().equals("/users/login")) {
+        if (req.getRequestURI().equals("/users/login") || req.getRequestURI().startsWith("/swagger-ui") || req.getRequestURI().startsWith("/v3")) {
             filterChain.doFilter(req, res);
             return;
         }
 
         String accessToken = jwtProvider.getAccessTokenFromHeader(req);
+
+        if (!StringUtils.hasText(accessToken)) {
+            log.error("토큰이 요청 헤더에 없습니다.");
+            jwtExceptionHandler(res, UNAUTHORIZED, "Access token is missing.");
+            return;
+        }
+
         String username = jwtProvider.getUsernameFromToken(accessToken);
 
-        if (StringUtils.hasText(accessToken)) {
-            if (jwtProvider.validateAccessToken(accessToken)) {
-                log.info("액세스 토큰 검증 성공");
-                updateToken(accessToken, username, res);
-            } else if (jwtProvider.hasRefreshToken(username)) {
+        // 액세스 토큰 검증
+        if (jwtProvider.validateAccessToken(accessToken)) {
+            log.info("액세스 토큰 검증 성공");
+            setAuthentication(username);
+        } else {
+            // 액세스 토큰이 만료되었을 때 Refresh 토큰 검증
+            if (jwtProvider.hasRefreshToken(username)) {
                 String refreshToken = jwtProvider.substringToken(
                         redisTemplate.opsForValue().get(username));
-                updateToken(refreshToken, username, res);
-                log.info("토큰 Refresh 성공");
+                if (!refreshToken.isEmpty()) {
+                    log.info("Refresh 토큰 검증 성공, 새로운 액세스 토큰 생성");
+                    updateAccessToken(refreshToken, username, res);
+                } else {
+                    log.error("Refresh 토큰이 유효하지 않음.");
+                    jwtExceptionHandler(res, UNAUTHORIZED, "Refresh token is invalid.");
+                    return;
+                }
             } else {
-                jwtExceptionHandler(res, UNAUTHORIZED);
+                log.error("유효하지 않은 토큰입니다.");
+                jwtExceptionHandler(res, UNAUTHORIZED, "Invalid token.");
                 return;
             }
-        } else {
-            log.error("토큰 없음");
         }
         filterChain.doFilter(req, res);
     }
@@ -77,27 +93,28 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                 userDetails.getAuthorities());
     }
 
-    private void jwtExceptionHandler(HttpServletResponse res, HttpStatus status) {
-        int statusCode = status.value();
-        res.setStatus(statusCode);
+    private void jwtExceptionHandler(HttpServletResponse res, HttpStatus status, String message) {
+        res.setStatus(status.value());
         res.setContentType("application/json");
         try {
-            String json = new ObjectMapper().writeValueAsString("invalid token error");
+            String json = new ObjectMapper().writeValueAsString(new ErrorResponse(message));
             res.getWriter().write(json);
-        } catch (Exception e) {
-            log.error(e.getMessage());
+        } catch (IOException e) {
+            log.error("응답 작성 중 오류: {}", e.getMessage());
         }
     }
 
-    private void updateToken(String token, String username, HttpServletResponse res)
+    private void updateAccessToken(String refreshToken, String username, HttpServletResponse res)
             throws IOException {
-        UserRole role = jwtProvider.getRoleFromToken(token);
+        UserRole role = jwtProvider.getRoleFromToken(refreshToken);
         String newAccessToken = jwtProvider.createAccessToken(username, role);
 
         res.setHeader(AUTHORIZATION_HEADER, newAccessToken);
-        setAuthentication(jwtProvider.getUsernameFromToken(newAccessToken));
+        setAuthentication(username);
 
         String jsonResponse = new ObjectMapper().writeValueAsString(
-                "new token generated : " + newAccessToken);
+                new AccessTokenDto(newAccessToken));
+        res.getWriter().write(jsonResponse);
+        log.info("새로운 액세스 토큰 생성 및 응답 전송 완료: {}", newAccessToken);
     }
 }
