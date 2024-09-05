50e3d180d7be1232b7b6aed3c9e163198e9972e4
leebeanbin <wjdqlsdy1004@naver.com>
2024-09-05 16:46:52 +0900

    오탈자 수정

--- src/main/java/com/sparta/internsecurity/user/repository/UserRepository.java
+++ src/main/java/com/sparta/internsecurity/user/repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.sparta.internsecurity.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepositroy extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
